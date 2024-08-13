394b62299595aa57e452a245ddbefddd89bca3b5
Raul Requena <requenaraul@gmail.com>
2024-08-13 14:29:20 -0400

    19. Auto transform payloads to DTO instances

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -25,13 +25,15 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeeService.findOne(id);
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeeService.findOne('' + id);
   }
 
   @Post()
-  create(@Body() CreateCoffeeDto: CreateCoffeeDto) {
-    return this.coffeeService.create(CreateCoffeeDto);
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    return this.coffeeService.create(createCoffeeDto);
   }
 
   @Patch(':id')
